c7e026ae4e16954ccc578f978f0021b9ae4bb835
Merksue <gabeaboy@gmail.com>
2017-05-17 20:57:38 -0400

    demo few conditionl/promp examples

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using helloworldd;
+
+namespace helloworld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Keep asking user until entered a num between 1-10
+
+            //var num = 0;
+            //do
+            //{
+            //    Console.Write("Enter a number between 1 and 10: ");
+            //    num = Convert.ToInt32(Console.ReadLine());
+            //} while (num > 10 || num < 1);
+            //Console.Write("Finally " + num);
+
+            //Find max of two values
+            //var x = 20;
+            //var y = 10;
+            //Console.Write("Max val is "+Math.Max(x,y));
+
+            //Another
+            //int limit;
+            //int speed;
+            //do
+            //{
+            //    Console.Write("Enter limit: ");
+            //    limit = Convert.ToInt32(Console.ReadLine());
+            //} while (limit < 0);
+            //Console.Write("Enter your speed: ");
+            //speed = Convert.ToInt32(Console.ReadLine());
+
+            //if (limit > speed)
+            //{
+            //    Console.Write("Under limit");
+            //}
+            //else if (limit == speed) Console.Write("Cruising speed");
+            //else Console.Write("Too fast, slow down");
+
+		}
+    }
+}
